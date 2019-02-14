b5f5209aacee5fba38fae90bb39de4ef37701183
Arie Septian <ariesptn@gmail.com>
2019-02-14 18:20:41 +0700

    image upload

--- routes/images.js
+++ routes/images.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const image = require('../controllers/images')
+const images = require('../helpers/images')
 
 router
   .get('/', image.getAll)
-  .post('/', image.create)
-  .put('/', image.update)
-  .delete('/', image.delete)
+  .post('/',
+    images.multer.single('articleFile'),
+    images.sendUploadToGCS,
+    image.create)
+  .put('/:imageId', image.update)
+  .delete('/:imageId', image.delete)
 
 module.exports = router;
